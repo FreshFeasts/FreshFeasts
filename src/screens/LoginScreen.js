fb0181c1e9066a25803fc7b118fcc5956eaf29a0
Girijesh Thodupunuri <girinuri123@gmail.com>
2023-07-30 16:26:46 -0700

    created context for login screen

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,14 +1,13 @@
-import {  Text, View, StatusBar } from "react-native";
+import { Text, View, StatusBar } from "react-native";
+import { LogInScreenContextProvider } from "../contexts/LogInScreenContext.jsx";
 
-
-const LoginScreen = ({navigation}) => {
-
+const LoginScreen = ({ navigation }) => {
   return (
-    <>
-    <View className="flex-1 items-center justify-center">
-      <Text>This is the login page</Text>
-    </View>
-    </>
+    <LogInScreenContextProvider>
+      <View className="flex-1 items-center justify-center">
+        <Text>This is the login page</Text>
+      </View>
+    </LogInScreenContextProvider>
   );
 };
 export default LoginScreen;
