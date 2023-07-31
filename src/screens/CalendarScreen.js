c9c380c4267ad06031454dbe2ae9243c8ebfae6d
Andy Moc <andymoc.com@gmail.com>
2023-07-31 13:34:20 -0700

    📆 Added Calendar Modal - Changing Stylling

--- src/screens/CalendarScreen.js
+++ src/screens/CalendarScreen.js
@@ -1,11 +1,11 @@
-import {  Text, View, StatusBar, TouchableOpacity} from "react-native";
+import {  Text, View, StatusBar, TouchableOpacity, Modal, Pressable, Image, StyleSheet} from "react-native";
 import {Calendar, LocaleConfig,Agenda, DateData, AgendaEntry, AgendaSchedule} from 'react-native-calendars';
 import React, {useState, useEffect} from 'react';
 
 
 const CalendarScreen = ({navigation}) => {
 
-  const [selected, setSelected] = useState('');
+  const [selected, setSelected] = useState(null);
   const [items, setItems] = useState({})
 
   const timeToString = (time) => {
@@ -38,36 +38,83 @@ const CalendarScreen = ({navigation}) => {
   };
 
   //old className style = className="flex-1 items-center justify-center"
-const renderItem = (item) => {
-  return(
-    <TouchableOpacity>
-      <Card>
-        <Card.Content>
-          <View>
-            <Text>
-              {item.name}
-            </Text>
+  const renderItem = (item) => {
+      return (
+        <Modal
+          animationType="slide"
+          transparent={true}
+          visible={selected !== null}
+          onRequestClose={() => {
+            setSelected(null);
+          }}
+        >
+          <View style={{ flex: 1 }}>
+            <View
+              style={{
+                margin: 20,
+                backgroundColor: "white",
+                borderRadius: 20,
+                padding: 35,
+                alignItems: "center",
+                shadowColor: "#000",
+                shadowOffset: {
+                  width: 0,
+                  height: 2,
+                },
+                shadowOpacity: 0.25,
+                shadowRadius: 4,
+                elevation: 5,
+              }}
+            >
+              <Text style={{ marginBottom: 15, textAlign: "center" }}>
+                Order Information
+              </Text>
+              <Pressable
+                style={[
+                  {
+                    borderRadius: 20,
+                    padding: 10,
+                    elevation: 2,
+                  },
+                  {
+                    backgroundColor: "#A5E06B",
+                  },
+                ]}
+                onPress={() => setSelected(null)}
+              >
+                <Text>Close</Text>
+              </Pressable>
+            </View>
           </View>
-        </Card.Content>
-      </Card>
-    </TouchableOpacity>
-  )
-}
+        </Modal>
+      )
+
+  }
+
   return (
     <>
-<<<<<<< HEAD
-    <View style ={{flex:1}}>
-    <Agenda
-    items={items}
-    loadItemsForMonth ={loadItems}
-    // renderItem={renderItem}
-    />
-=======
-    <View className="flex-1 items-center justify-center">
-      <Text className="font-main">This is the Calendar page</Text>
->>>>>>> dev
-    </View>
+      <View style={{ flex: 1 }}>
+        <Calendar
+          items={items}
+          loadItemsForMonth={loadItems}
+          onDayPress={(day) => {
+            setSelected(day.dateString); // Set the selected state to the date string
+          }}
+          markedDates={{
+            [selected]: {
+              selected: true,
+              disableTouchEvent: true,
+              selectedColor: "#238A28",
+              todayTextColor: '#black',
+              dayTextColor: '#2d4150',
+              selectedDayTextColor: '#ffffff',
+              marked: true,
+            },
+          }}
+        />
+      </View>
+      {renderItem()}
     </>
-  );
-};
+  )
+}
 export default CalendarScreen;
