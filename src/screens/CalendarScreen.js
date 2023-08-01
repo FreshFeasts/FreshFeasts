49690349166bf086cf23ee8e0bb1f2cb584dfad0
Andy Moc <andymoc.com@gmail.com>
2023-08-01 11:00:26 -0700

    📆 Connecting to Server Soon

--- src/screens/CalendarScreen.js
+++ src/screens/CalendarScreen.js
@@ -1,4 +1,4 @@
-import {  Text, View, StatusBar, TouchableOpacity, Modal, Pressable, Image, StyleSheet} from "react-native";
+import {  Text, View, StatusBar, TouchableOpacity, Modal, Pressable, Image, StyleSheet, TouchableWithoutFeedback} from "react-native";
 import {Calendar, LocaleConfig,Agenda, DateData, AgendaEntry, AgendaSchedule} from 'react-native-calendars';
 import React, {useState, useEffect} from 'react';
 
@@ -8,38 +8,35 @@ const CalendarScreen = ({navigation}) => {
   const [selected, setSelected] = useState(null);
   const [items, setItems] = useState({})
 
-  const timeToString = (time) => {
-    const date = new Date(time);
-    return date.toISOString().split('T')[0];
-  }
-
-  const loadItems = (day) => {
-    setTimeout(() => {
-      for (let i = -15; i < 85; i++) {
-        const time = day.timestamp + i * 24 * 60 * 60 * 1000;
-        const strTime = timeToString(time);
-        if (!items[strTime]) {
-          items[strTime] = [];
-          const numItems = Math.floor(Math.random() * 3 + 1);
-          for (let j = 0; j < numItems; j++) {
-            items[strTime].push({
-              name: 'Item for ' + strTime + ' #' + j,
-              height: Math.max(50, Math.floor(Math.random() * 150)),
-            });
-          }
-        }
-      }
-      const newItems = {};
-      Object.keys(items).forEach((key) => {
-        newItems[key] = items[key];
-      });
-      setItems(newItems);
-    }, 1000);
+  //SAMPLE DATA TO TEST GET REQUESTS
+  const orders = {
+    _id: '1',
+    userId: '1',
+    meals: ['Skittles','Gushers','Calimari','Sushi'],
+    deliveryDate: '08/11/2023',
   };
 
-  //old className style = className="flex-1 items-center justify-center"
-  const renderItem = (item) => {
+
+  const meals = orders.meals.map((food,index) => {
+    //adding 1 to index so users will not see the 0th index at start and see #1.
+    return(
+      <>
+      <Text>Meal #{index+1}: {food}</Text>
+      </>
+    )
+  })
+
+  const showNestedModal = () => {
+    setNestedModalVisible(true);
+  };
+
+  const closeNestedModal = () => {
+    setNestedModalVisible(false);
+  };
+
+  const renderItem = () => {
       return (
+        <View>
         <Modal
           animationType="slide"
           transparent={true}
@@ -48,6 +45,8 @@ const CalendarScreen = ({navigation}) => {
             setSelected(null);
           }}
         >
+            <Pressable style={{ flex: 1}} onPress={() => setSelected(null)}>
+
           <View style={{ flex: 1 }}>
             <View
               style={{
@@ -69,50 +68,55 @@ const CalendarScreen = ({navigation}) => {
               <Text style={{ marginBottom: 15, textAlign: "center" }}>
                 Order Information
               </Text>
-              <Pressable
-                style={[
-                  {
-                    borderRadius: 20,
-                    padding: 10,
-                    elevation: 2,
-                  },
+              <Text>Delivery Date: {orders.deliveryDate}</Text>
+              {meals}
+              <TouchableOpacity style={[
+                {
+                  borderRadius: 15,
+                  padding: 10,
+                  elevation: 5,
+                },
                   {
                     backgroundColor: "#A5E06B",
                   },
-                ]}
-                onPress={() => setSelected(null)}
-              >
-                <Text>Close</Text>
-              </Pressable>
+                ]}>
+              <Text>Edit Delivery Date</Text>
+              </TouchableOpacity>
             </View>
           </View>
+        </Pressable>
         </Modal>
+        </View>
       )
 
   }
 
   return (
     <>
-      <View style={{ flex: 1 }}>
+      <View style={{ flex: 1}}>
         <Calendar
-          items={items}
-          loadItemsForMonth={loadItems}
+
+          //Send it as an object or JSON
+          //maybe day can be the delivery dates so that users cannot select other dates with a pop up modal
+
           onDayPress={(day) => {
-            setSelected(day.dateString); // Set the selected state to the date string
+            setSelected(day.dateString)
           }}
           markedDates={{
             [selected]: {
               selected: true,
               disableTouchEvent: true,
               selectedColor: "#238A28",
-              todayTextColor: '#black',
-              dayTextColor: '#2d4150',
+              todayTextColor: '#FFA500',
+              dayTextColor: '#FFA500',
               selectedDayTextColor: '#ffffff',
               marked: true,
             },
           }}
         />
       </View>
+
+      {/* Conditionally render items for delivery dates */}
       {renderItem()}
     </>
   )
