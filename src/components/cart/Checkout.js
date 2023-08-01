abf6294868b137da61efde01f95e19baa3ccafa4
carolinerobbins <caroline.tawney@gmail.com>
2023-08-01 16:05:10 -0700

    💰completed api links to add meal to cart and checkout with cart

--- src/components/cart/Checkout.js
+++ src/components/cart/Checkout.js
@@ -1,45 +1,96 @@
-import React, {useState} from 'react';
-import {  Pressable, Text, View, ScrollView } from "react-native";
-import AppText from '../../utils/components/AppText';
-import CartCard from './CartCard'
+import React, { useState, useContext, useEffect } from "react";
+import { Pressable, Text, View, ScrollView } from "react-native";
+import AppText from "../../utils/components/AppText";
+import CartCard from "./CartCard";
+import { LogInScreenContext } from "../../contexts/LogInScreenContext.jsx";
+import { getUser, postCart, getMeals } from "../../utils/apis/api";
 
 const Checkout = () => {
-  const cart = {
-    deliveryDate: 'August 1, 2023', //will update with proper dateFNS
-    orderDate: 'July 30, 2023',
-    meals: [{
-      name: 'Spaghetti',
-      url: 'https://images.pexels.com/photos/128408/pexels-photo-128408.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
-    }, {
-      name: 'Calamari',
-      url: 'https://images.pexels.com/photos/4181933/pexels-photo-4181933.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
-    }, {
-      name: 'Crabby Pattie',
-      url: "https://static.wikia.nocookie.net/spongebob/images/0/0b/The_Krabby_Patty_That_Ate_Bikini_Bottom_072.png/revision/latest/scale-to-width-down/1000?cb=20221028203009"
-    }]
-  }
+  const email = "Enid.Johns@yahoo.com";
+  const [cartMeals, setCartMeals] = useState([]);
+  const [cart, setCart] = useState({
+    userId: "64c96db323bfcbd4a7159209",
+    meals: [
+      "64c59c1d32ab87d43f2bec71",
+      "64c5c4af32ab87d43f2bec79",
+      "64c5c4af32ab87d43f2bec80",
+    ],
+    deliveryDate: "08/04/23",
+    orderDate: new Date(),
+  });
+  const [user, setUser] = useState({});
+  const [loading, setLoading] = useState(true);
 
   const submitOrder = () => {
-    console.log("order submitted")
-  }
+    let results = postCart(cart);
+    console.log(results);
+  };
+
+  useEffect(() => {
+    getUser(email).then((data) => {
+      const mealList = data.currentCart.meals;
+      setCart(data.currentCart);
+      setUser(data);
+      getMeals()
+        .then((meals) => {
+          const mealDetails = meals.filter((item) => {
+            return mealList.includes(item._id);
+          });
+          setCartMeals(mealDetails);
+          setLoading(false);
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    });
+  }, []);
 
   return (
-    <View className="flex justify-start">
-      <View className="bg-maize items-center justify-center">
-      <AppText className="text-3xl text-pakistangreen ml-2 my-4 " style={{ fontFamily: 'ComfortaaBold' }}>Order Summary</AppText>
-      </View>
-      <AppText className="text-2xl text-pakistangreen ml-1 mt-2"> Meals: </AppText>
-      <ScrollView bounces={false}>
-      {cart.meals.map((meal) => <CartCard meal={meal} key={meal.name}/>
+    <>
+      {loading ? (
+        <Text>Cart is loading...</Text>
+      ) : (
+        <View className="flex justify-start">
+          <View className="bg-maize items-center justify-center">
+            <AppText
+              className="text-3xl text-pakistangreen ml-2 my-4 "
+              style={{ fontFamily: "ComfortaaBold" }}
+            >
+              Order Summary
+            </AppText>
+          </View>
+          <View className="flex justify-end absolute top-20 right-2">
+            <AppText>
+              {user.firstName} {user.lastName}
+            </AppText>
+            <AppText>{user.email}</AppText>
+          </View>
+          <AppText className="text-2xl text-pakistangreen ml-1 mt-2">
+            Meals:
+          </AppText>
+          <ScrollView bounces={false}>
+            {cartMeals.map((meal) => (
+              <CartCard meal={meal} key={meal.name} />
+            ))}
+          </ScrollView>
+          <AppText className="text-xl text-pakistangreen ml-1 mt-2">
+            Delivery Date: {cart.deliverDate}
+          </AppText>
+          <AppText className="text-xl text-pakistangreen ml-1 mt-2">
+            Payment Information: On File
+          </AppText>
+          <Pressable
+            className="items-center justify-center rounded-md"
+            onPress={submitOrder}
+          >
+            <AppText className="text-2xl bg-pakistangreen text-lemonchiffon p-4 m-4">
+              Submit Order
+            </AppText>
+          </Pressable>
+        </View>
       )}
-      </ScrollView>
-      <AppText className="text-xl text-pakistangreen ml-1 mt-2"> Delivery Date: {cart.deliveryDate} </AppText>
-      <AppText className="text-xl text-pakistangreen ml-1 mt-2"> Payment Information: On File </AppText>
-      <Pressable className="items-center justify-center rounded-md" onPress={submitOrder}>
-        <AppText className="text-2xl bg-pakistangreen text-lemonchiffon p-4 m-4">Submit Order</AppText>
-      </Pressable>
-    </View>
-  )
-}
+    </>
+  );
+};
 
 export default Checkout;
--- src/components/home/MealModal.js
+++ src/components/home/MealModal.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Modal, Pressable, Text, View, Image } from "react-native";
 import Icon from "react-native-vector-icons/FontAwesome";
 import Nutrition from "./Nutrition";
-import { getMealDetails, updateCart } from "../../utils/apis/api";
+import { getUser, updateCart } from "../../utils/apis/api";
 import DietChip from "../../utils/components/DietChip";
 import AppText from "../../utils/components/AppText";
 import CartIncrementer from "../../utils/components/CartIncrementer";
@@ -10,7 +10,10 @@ import { calcAverageRating } from "../../utils/helpers";
 
 const MealModal = ({ mealSelection, handleSelectMeal }) => {
   const [modalVisible, setModalVisible] = useState(false);
+  const email = "Enid.Johns@yahoo.com";
   const [added, setAdded] = useState(false);
+  const [cart, setCart] = useState({});
+  const [user, setUser] = useState({});
   const [meal, setMeal] = useState({
     _id: "64c59c1d32ab87d43f2bec71",
     name: "Chef Basil's Bliss",
@@ -73,6 +76,29 @@ const MealModal = ({ mealSelection, handleSelectMeal }) => {
     }
   }, [mealSelection]);
 
+  useEffect(() => {
+    getUser(email)
+      .then((data) => {
+        setCart(data.currentCart);
+        setUser(data);
+        if (data.currentCart.meals.includes(meal._id)) {
+          setAdded(true);
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
+
+  const handleAddMeal = () => {
+    const userId = user._id
+    const update = { ...cart, userId };
+    update.meals.push(meal._id)
+    setCart(update);
+    updateCart(update);
+    setAdded(true);
+  };
+
   return (
     <View>
       <Modal
@@ -93,7 +119,10 @@ const MealModal = ({ mealSelection, handleSelectMeal }) => {
                 <Icon name="times-circle" size={24} color="white" />
               </Pressable>
             </View>
-            <AppText className="font-[bold] text-xl text-white m-2" style={{ fontFamily: 'ComfortaaBold' }}>
+            <AppText
+              className="font-[bold] text-xl text-white m-2"
+              style={{ fontFamily: "ComfortaaBold" }}
+            >
               {meal.name}
             </AppText>
             <Image
@@ -114,7 +143,7 @@ const MealModal = ({ mealSelection, handleSelectMeal }) => {
               <View className="h-8 w-32 border-2 border-pakistangreen justify-center items-center bg-lemonchiffon rounded-2 m-1 p-1">
                 <AppText className="text-xs">
                   Rating: {calcAverageRating(meal.ratings)}
-                   <Icon name="star" size={12} color="#0E4000" />
+                  <Icon name="star" size={12} color="#0E4000" />
                 </AppText>
               </View>
             </View>
@@ -130,27 +159,31 @@ const MealModal = ({ mealSelection, handleSelectMeal }) => {
             </View>
             <Nutrition nutrition={meal.nutrition} />
             <View className="absolute bottom-2 left-2 w-56">
-              <AppText className='text-xs text-white'>{meal.favorites} other FreshFeast customers favorited this meal!</AppText>
+              <AppText className="text-xs text-white">
+                {meal.favorites} other FreshFeast customers favorited this meal!
+              </AppText>
             </View>
-            {added ?
-            <View className="absolute bottom-2 right-2">
-            <CartIncrementer added={added} setAdded={setAdded} color="white"/>
-            </View>
-            :<View className="absolute bottom-2 right-2">
-              <Pressable
-                className="flex-row items-center"
-                onPress={() => {
-                  updateCart(meal.mealId);
-                  setAdded(true);
-                }}
-                // need to decide action after adding meal
-                // options: bring back to meal list
-                // or change to an incrementer with 1 in cart
-              >
-                <Text className="font-main text-white mr-2">Add to Cart</Text>
-                <Icon name="cart-plus" size={32} color="white" />
-              </Pressable>
-            </View> }
+            {added ? (
+              <View className="absolute bottom-2 right-2">
+                <CartIncrementer
+                  added={added}
+                  setAdded={setAdded}
+                  color="white"
+                  cart={cart}
+                  setCart={setCart}
+                />
+              </View>
+            ) : (
+              <View className="absolute bottom-2 right-2">
+                <Pressable
+                  className="flex-row items-center"
+                  onPress={handleAddMeal}
+                >
+                  <Text className="font-main text-white mr-2">Add to Cart</Text>
+                  <Icon name="cart-plus" size={32} color="white" />
+                </Pressable>
+              </View>
+            )}
           </View>
         </View>
       </Modal>
