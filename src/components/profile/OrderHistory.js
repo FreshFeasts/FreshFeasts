f858abaae7e5f78366ccc5ac6ffdce4f9b47f25b
carolinerobbins <caroline.tawney@gmail.com>
2023-08-01 19:58:39 -0700

    fixed rendering of multiple orders and included cards with pictures

--- src/components/profile/OrderHistory.js
+++ src/components/profile/OrderHistory.js
@@ -1,43 +1,75 @@
-import React, {useState, useContext, useEffect} from 'react';
-import {  Pressable, ScrollView, Text, View } from "react-native";
-import HistoryCard from './HistoryCard';
-import AppText from '../../utils/components/AppText'
-import {getUser, getOrders, getMeals } from "../../utils/apis/api";
-import { LogInScreenContext } from '../../contexts/LogInScreenContext.jsx'
-import { format, parseISO } from 'date-fns';
+import React, { useState, useContext, useEffect } from "react";
+import { Pressable, ScrollView, Text, View } from "react-native";
+import HistoryCard from "./HistoryCard";
+import AppText from "../../utils/components/AppText";
+import { getUser, getOrders, getMeals } from "../../utils/apis/api";
+import { LogInScreenContext } from "../../contexts/LogInScreenContext.jsx";
+import { format, parseISO } from "date-fns";
 
-const OrderHistory = ({ history, setHistory}) => {
-  const userId = '64c96db323bfcbd4a7159209';
-  const [ orders, setOrders ] = useState(null);
+const OrderHistory = ({ history, setHistory }) => {
+  const userId = "64c96db323bfcbd4a7159209";
+  const [orders, setOrders] = useState(null);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-        getOrders(userId). then((orderData) => {
-        setOrders(orderData);
-        setLoading(false);
-       }).catch((err) => {
-        console.error(err);
-      })
-  },[])
+    getOrders(userId).then((orderData) => {
+      getMeals()
+        .then((meals) => {
+          const ordersMealDetail = orderData.map((order) => {
+            const mealDetails = meals.filter((meal) => order.meals.includes(meal._id));
+            const mealsWithDetails = mealDetails.map((meal) => {
+              return {
+                name: meal.name,
+                photo: meal.photo,
+              };
+            });
+            return {
+              ...order,
+              meals: mealsWithDetails,
+            };
+
+          });
+          setOrders(ordersMealDetail);
+          setLoading(false);
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    });
+  }, []);
 
   return (
     <>
-    {loading ? <Text>Orders are loading...</Text> :
-    <ScrollView bounces={false}>
-    <View className="flex-1 justify-center items-center">
-      {orders.map((order) => {
-        const formattedDate = format(parseISO(order.deliveryDate), 'MM/dd/yyyy');
-       return (
-        <>
-       <AppText className="text-xl" key={order.orderId}>{formattedDate}</AppText>
-      {order.meals.map((meal) =><HistoryCard key={meal.name} meal={meal}/>)}
-      </>)
-    })}
-      <Pressable className="px-4 py-2 bg-pakistangreen rounded-md" onPress={() => setHistory(!history)}>
-          <AppText className="text-lemonchiffon"> Close History </AppText>
-        </Pressable>
-    </View>
-    </ScrollView>}
+      {loading ? (
+        <Text>Orders are loading...</Text>
+      ) : (
+        <ScrollView bounces={false}>
+          <View className="flex-1 justify-center items-center">
+            {orders.map((order) => {
+              const formattedDate = format(
+                parseISO(order.deliveryDate),
+                "MM/dd/yyyy"
+              );
+              return (
+                <>
+                  <AppText className="text-xl" key={order.orderId}>
+                    {formattedDate}
+                  </AppText>
+                  {order.meals.map((meal) => (
+                    <HistoryCard key={meal.name} meal={meal} />
+                  ))}
+                </>
+              );
+            })}
+            <Pressable
+              className="px-4 py-2 bg-pakistangreen rounded-md"
+              onPress={() => setHistory(!history)}
+            >
+              <AppText className="text-lemonchiffon"> Close History </AppText>
+            </Pressable>
+          </View>
+        </ScrollView>
+      )}
     </>
   );
 };
