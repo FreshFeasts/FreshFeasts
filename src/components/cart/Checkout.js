29194514e4c7d7cd989b4ae6dfaa2fe6cd17992b
carolinerobbins <caroline.tawney@gmail.com>
2023-08-02 11:40:22 -0700

    🛒 added delivery picker, cleaned up formatting

--- src/components/cart/Checkout.js
+++ src/components/cart/Checkout.js
@@ -11,7 +11,7 @@ import {
   getPayment,
 } from "../../utils/apis/api";
 import { format, parseISO } from "date-fns";
-import DropDownPicker from 'react-native-dropdown-picker';
+import DropDownPicker from "react-native-dropdown-picker";
 
 const Checkout = () => {
   const email = "Enid.Johns@yahoo.com";
@@ -21,20 +21,28 @@ const Checkout = () => {
   const [loading, setLoading] = useState(true);
   const [payment, setPayment] = useState();
   const [address, setAddress] = useState({
-    address1: '',
-    address2: '',
-    city: '',
-    state: '',
-    zip: ''
+    address1: "",
+    address2: "",
+    city: "",
+    state: "",
+    zip: "",
   });
   const cost = 9.99;
-
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState(null);
+  const [items, setItems] = useState([
+    { label: "Monday", value: "0" },
+    { label: "Tuesday", value: "1" },
+    { label: "Wednesday", value: "2" },
+    { label: "Thursday", value: "3" },
+    { label: "Friday", value: "4" },
+  ]);
 
   const submitOrder = () => {
     const final = {
       userId: user._id,
-      currentCart: {...cart, orderDate: Date.now()}
-    }
+      currentCart: { ...cart, orderDate: Date.now() },
+    };
     let results = postCart(final);
   };
 
@@ -63,7 +71,7 @@ const Checkout = () => {
       let contact = await getUserContact(userId);
       let payments = await getPayment(userId);
       let card = payments[0].ccId.toString();
-      let last4 = '************' + card.slice(card.length - 4);
+      let last4 = "************" + card.slice(card.length - 4);
       setAddress(contact.deliveryAddress);
       setPayment(last4);
     } catch (err) {
@@ -79,53 +87,82 @@ const Checkout = () => {
         <View className="flex flex-col">
           <View className="bg-pakistangreen items-center justify-center">
             <AppText
-              className="text-3xl text-white ml-2 my-4"
+              className="text-3xl text-white ml-2 my-2"
               style={{ fontFamily: "ComfortaaBold" }}
             >
               Order Summary
             </AppText>
           </View>
-          <View className="flex justify-end ml-2 mt-2">
-          <AppText className="text-2xl text-pakistangreen my-2">
-            Customer Information
-          </AppText>
-            <AppText className='mb-1'>
+          <View className="flex justify-end ml-2 mt-1">
+            <AppText className="text-xl text-pakistangreen my-2">
+              Customer Information
+            </AppText>
+            <View className="flex-row">
+            <AppText className="mb-1">
               {user.firstName} {user.lastName}
             </AppText>
-            <AppText className='mb-1'>{user.email}</AppText>
-            <AppText className='mb-1'>{address.address1}</AppText>
-            {address.address2 === '' ? null : <AppText>{address.address2}</AppText>}
-            <View className="flex-row">
-            <AppText className='mb-1'>{address.city}, {address.state} {address.zip} </AppText>
+            <AppText className="mb-1 absolute right-0">{user.email}</AppText>
             </View>
-            <AppText className='mb-1'>Card on File: {payment}</AppText>
+            <AppText className="mb-1">{address.address1}</AppText>
+            {address.address2 === "" ? null : (
+              <AppText>{address.address2}</AppText>
+            )}
+              <AppText className="mb-1">
+                {address.city}, {address.state} {address.zip}{" "}
+              </AppText>
+            <AppText className="mb-1">Card on File: {payment}</AppText>
           </View>
-          <AppText className="text-2xl text-pakistangreen ml-1 mt-2">
+          {/* <AppText className="text-xl text-pakistangreen ml-1 mt-1">
             Meals
-          </AppText>
-          <ScrollView bounces={false} className="h-[25%]" >
+          </AppText> */}
+          <View className="bg-pakistangreen h-1 mt-1" />
+          <ScrollView bounces={false} className="h-[40%]">
             {cartMeals.map((meal) => (
-              <CartCard meal={meal} key={meal.name} cart={cart} setCart={setCart}/>
+              <CartCard
+                meal={meal}
+                key={meal.name}
+                cart={cart}
+                setCart={setCart}
+              />
             ))}
           </ScrollView>
           <View className="bg-pakistangreen h-1 mt-1" />
-          <AppText className="text-lg text-pakistangreen ml-1 mt-2">
-            Delivery Date: {format(parseISO(cart.deliveryDate),"MM/dd/yyyy")}
+          <View className="flex-row items-center">
+          <AppText className="text-base text-pakistangreen mx-1 my-2">
+            Delivery Date: {format(parseISO(cart.deliveryDate), "MM/dd/yyyy")}
           </AppText>
-          <AppText className="text-lg text-pakistangreen ml-1 mt-2">
+          <DropDownPicker
+            placeholderStyle={{
+              color: "black",
+            }}
+            containerStyle={{
+              width: 150,
+              height: 20
+            }}
+            labelStyle={{
+              textAlign: "center",
+            }}
+            placeholder="Change day"
+            open={open}
+            value={value}
+            items={items}
+            setOpen={setOpen}
+            setValue={setValue}
+            setItems={setItems}
+          />
+          </View>
+          <AppText className="text-base text-pakistangreen ml-1 mt-2">
             Total Meals: {cart.meals.length}
           </AppText>
-          <AppText className="text-lg text-pakistangreen ml-1 mt-2">
+          <AppText className="text-base text-pakistangreen ml-1 mt-2">
             Weekly Cost: ${cart.meals.length * cost}
           </AppText>
           <View className="justify-end items-center rounded-md">
-          <Pressable
-            onPress={submitOrder}
-          >
-            <AppText className="text-2xl bg-pakistangreen text-white p-2 m-2">
-              Submit Order
-            </AppText>
-          </Pressable>
+            <Pressable onPress={submitOrder}>
+              <AppText className="text-2xl bg-pakistangreen text-white p-2 m-2">
+                Submit Order
+              </AppText>
+            </Pressable>
           </View>
         </View>
       )}
