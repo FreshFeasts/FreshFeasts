2fd0318db1a42a2fbecb522f328bcaaf248d309e
Tim Engle <timothy.englew@gmail.com>
2023-08-03 13:45:03 -0700

    💬 Update Chat Syling

--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -1,15 +1,31 @@
-import React, { useEffect, useState } from 'react';
-import { Dimensions, Text, View, StyleSheet, Image, Pressable, TextInput, Button, FlatList, SafeAreaView } from 'react-native';
+import React, { useContext, useEffect, useState } from 'react';
+import {
+  Dimensions,
+  Text,
+  View,
+  StyleSheet,
+  Image,
+  Pressable,
+  TextInput,
+  Button,
+  FlatList,
+  SafeAreaView,
+  TouchableOpacity,
+  ScrollView,
+} from 'react-native';
 import io from 'socket.io-client';
 // const socket = io("http://44.211.123.112:3005");
 // import ViewPropTypes from 'deprecated-react-native-prop-types';
 // import Carousel, { Pagination } from 'react-native-snap-carousel';
-import AppText from '../../utils/components/AppText'
+import AppText from '../../utils/components/AppText';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { LogInScreenContext } from '../../contexts/LogInScreenContext';
+import { formatDistanceToNow } from 'date-fns';
+
 // import MealModal from './MealModal';
 
 const SOCKET_SERVER_URL = 'http://44.211.123.112:3005'; // Replace with your socket server URL
 const socket = io(SOCKET_SERVER_URL);
-
 
 socket.on('connect', () => {
   console.log('Connected to server');
@@ -24,30 +40,41 @@ socket.on('chat message', (msg) => {
 });
 
 const sendMessage = (msg) => {
-  socket.emit('chat message', {"sender": 'chen', "action": "broadcast", "msg": msg});
+  socket.emit('chat message', {
+    sender: 'chen',
+    action: 'broadcast',
+    time: new Date(),
+    msg: msg,
+  });
   // socket.emit("broadcast", {"sender": 'chen', "action": "broadcast", "msg": msg});
-  console.log('user sent:', msg)
+  console.log('user sent:', msg);
 };
 
 // display messages of other clients in our client
-socket.on("broadcast", (data) => {
-  console.log(data.msg)
+socket.on('broadcast', (data) => {
+  console.log(data.msg);
 });
 
-
 const Chat = () => {
-  const [messages, setMessages] = useState([{
-    msg: 'hi im the nutritionist',
-    sender: 'nutritionist'
-  },{
-    msg: 'what can I help you',
-    sender: 'nutritionist'
-  },
-  {
-    msg: 'yooooooo yooooooo yooooooo yooooooo yooooooo yooooooo yooooooo',
-    sender: 'chen'
-  }
-]);
+  // const { userInitData } = useContext(LogInScreenContext);
+  const [messages, setMessages] = useState([
+    {
+      // msg: `hi ${userInitData.info.firstName} im the nutritionist`,
+      msg: `hi  im the nutritionist`,
+      sender: 'nutritionist',
+      time: new Date(),
+    },
+    {
+      msg: 'what can I help you',
+      sender: 'nutritionist',
+      time: new Date(),
+    },
+    {
+      msg: 'yooooooo yooooooo yooooooo yooooooo yooooooo yooooooo yooooooo',
+      sender: 'chen',
+      time: new Date(),
+    },
+  ]);
   const [message, setMessage] = useState('');
 
   useEffect(() => {
@@ -67,79 +94,63 @@ const Chat = () => {
         <Text>{item.msg}</Text>
         <Text>{item.sender}</Text>
       </View>
-    )
-  }
+    );
+  };
 
   return (
-    <SafeAreaView style={styles.container}>
-      <View className=''>
-        <View>
-          <Text>Chat Screen</Text>
-        </View>
-        <View className=''>
-          {/* {
-            messages[0] &&
-            <FlatList
-              style={styles.item}
-              data={messages}
-              renderItem={renderItem}
-            />
-          } */}
-          {
-            messages.map((item, index) => {
-              return (
-                <Message key={index} item={item}/>
-              )
-            })
-          }
-        </View>
-        <View className='flex-row justify-center'>
-          <TextInput
-            className='w-80 bg-white h-8 '
-            value={message}
-            onChangeText={setMessage}
-          />
-          <Button
-            title="Send"
-            onPress={handleSendMessage}
-          />
-        </View>
-        {/* Add your components and UI elements here */}
+    // <SafeAreaView style={styles.container}>
+    <View className="h-[87.5%] bg-inherit">
+      <ScrollView className="">
+        {messages.map((item, index) => {
+          return <Message key={index} item={item} />;
+        })}
+      </ScrollView>
+      <View className="flex-row items-center px-4 mt-auto justify-between">
+        <TextInput
+          className=" flex w-[87.5%] px-2 h-10 bg-white  rounded-lg text-xl  "
+          value={message}
+          onChangeText={setMessage}
+        />
+        <TouchableOpacity
+          className="flex items-center justify-center h-10 bg-blue-600 px-2 py-1 rounded full  "
+          onPress={handleSendMessage}
+        >
+          {/* <Text className="text-lg text-white">Send</Text> */}
+          <Icon name={'arrow-up'} size={20} color="white" />
+        </TouchableOpacity>
       </View>
-    </SafeAreaView>
+      {/* Add your components and UI elements here */}
+    </View>
+    // </SafeAreaView>
   );
 };
 
 const Message = ({ item }) => {
   return (
-    <View className={
-      item.sender === 'nutritionist' ? (
-        'w-screen mb-2 items-start'
-      )
-      : (
-        'w-screen mb-2 items-end'
-      )
-    }>
+    <View
+      className={`px-2  ${
+        item.sender === 'nutritionist'
+          ? 'w-screen mb-2 items-start'
+          : 'w-screen mb-2 items-end'
+      }`}
+    >
       <View
         style={styles.item}
-        className={
-          item.sender === 'nutritionist' ? (
-            'bg-gray-300 rounded-lg p-4 mb-2 ml-2'
-          )
-          : (
-            'bg-lemonchiffon rounded-lg p-4 mr-2'
-          )
-        }>
-        <AppText className='text-pakistangreen'>
-          {item.msg}
-        </AppText>
+        className={`
+          ${
+            item.sender === 'nutritionist'
+              ? 'bg-gray-300 rounded-lg p-4 mb-2 '
+              : 'bg-lemonchiffon rounded-lg p-4 '
+          }`}
+      >
+        <AppText className="text-pakistangreen">{item.msg}</AppText>
       </View>
-        <AppText className='text-pakistangreen'>
-          {item.sender}
-        </AppText>
+      <AppText className="text-pakistangreen">
+        {formatDistanceToNow(new Date(item.time))}
+      </AppText>
     </View>
-  )
-}
+  );
+};
 const styles = StyleSheet.create({
   // container: {
   //   flexDirection: 'row',
@@ -169,7 +180,7 @@ const styles = StyleSheet.create({
     // backgroundColor: '#f9c2ff',
     padding: 20,
     marginVertical: 8,
-    maxWidth: 300
+    maxWidth: 300,
     // marginHorizontal: 16,
   },
   // text: {
--- src/screens/ChatScreen.js
+++ src/screens/ChatScreen.js
@@ -1,16 +1,22 @@
-import {  Text, View, StatusBar } from "react-native";
+import { Text, View, StatusBar, Image, ImageBackground } from 'react-native';
 import Chat from '../components/chat/Chat.js';
+import AppText from '../utils/components/AppText.js';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import chefThumb from '../../assets/images/cbthumb.png';
 
-
-const ChatScreen = ({navigation}) => {
-
+const ChatScreen = ({ navigation }) => {
   return (
-    <>
-    <View className="flex-1 items-center justify-center">
-      <Text className="font-main">This is the Chat page</Text>
-      <Chat/>
+    <View className="flex flex-col items-center h-full">
+      <View className="w-full h-[15%] flex items-center justify-center  bg-forestgreen drop-shadow-md">
+        <View className="overflow-hidden rounded-[50%] h-20 w-20 self-center">
+          <ImageBackground
+            source={chefThumb}
+            className="bg-contain h-20 w-20 "
+          />
+        </View>
+      </View>
+      <Chat />
     </View>
-    </>
   );
 };
 export default ChatScreen;
