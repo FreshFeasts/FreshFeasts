3e7745e2d371aa41bc540059eabe30e64f2e4061
Girijesh Thodupunuri <girinuri123@gmail.com>
2023-08-04 10:08:09 -0700

    fixed merge conflicts

--- src/components/profile/ProfileComponent.jsx
+++ src/components/profile/ProfileComponent.jsx
@@ -204,7 +204,7 @@ const ProfileComponent = ({ setLoggedIn }) => {
           <Text className="text-lemonchiffon font-main">Save</Text>
         </Pressable>
         <TouchableOpacity className="px-4 py-4 bg-black rounded-md" onPress={onSignOutHandler}>
-          <Text className="text-white font-main">Sing Out</Text>
+          <Text className="text-white font-main">Sign Out</Text>
         </TouchableOpacity>
       </View>
     </>
